ce74a25349620ca9f0feba3f115a9d2b3807ba26
Erick Yellott <erick.yellott@gmail.com>
2015-03-08 11:25:30 -0500

    ex1: Extra credit.
    Printing more things with `puts`.

--- ex1.c
+++ ex1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    puts("Hello world.");
+    puts("Hello world.\n");
+
+    // Extra credit
+    puts("worker bees can leave");
+    puts("even drones can fly away");
+    puts("the queen is their slave");
 
     return 0;
 }
