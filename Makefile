d5d20b4d680fce2e02f033bad6d5e565f2425fb1
Erick Yellott <erick.yellott@gmail.com>
2015-06-29 21:12:18 -0500

    ex9: Introduction to strings.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CFLAGS=-Wall -g -O0
 
-all: ex1 ex3 ex4 ex5 ex6 ex7 ex8
+all: ex1 ex3 ex4 ex5 ex6 ex7 ex8 ex9
 
 clean:
 	rm -rf ex? *.dSYM
---
+++ ex9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(int argc, char*argv[]) {
+    int numbers[4] = {0};  // With 1 value, everything else is initialized to zero.
+    char name[4] = {'a'};
+
+    // First, print them out raw
+    printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+    printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+
+    printf("name: %s\n", name);
+
+    // Setup the numbers
+    numbers[0] = 1;
+    numbers[1] = 2;
+    numbers[2] = 3;
+    numbers[3] = 4;
+
+    // Setup the name
+    name[0] = 'Z';
+    name[1] = 'e';
+    name[2] = 'd';
+    name[3] = '\0';
+
+    // Then print them out initialized
+    printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+    printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+
+    // Print the name like a string
+    printf("name: %s\n", name);
+
+    // Anotehr way to use name
+    char *another = "Zed";
+
+    printf("another: %s\n", another);
+    printf("another each: %c %c %c %c\n", another[0], another[1], another[2], another[3]);
+
+    int test[] = {};
+    printf("%d %d %d %d ", test[0], test[1], test[2], test[3]);  // Not initialized to 0
+
+    return 0;
+}
