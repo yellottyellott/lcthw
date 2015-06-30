9a40bd757b3f9ef962957afefb5c241becc24a20
Erick Yellott <erick.yellott@gmail.com>
2015-06-29 21:49:34 -0500

    ex10: Breaking it.
    Trying to loop past the end of an array results in a segfault.

--- ex10.c
+++ ex10.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
         "California", "Oregon",
         "Washington", "Texas"
     };
-    int num_states = 4;
+    int num_states = 10;
     for(i = 0; i < num_states; i++) {
         printf("state %d: %s\n", i, states[i]);
     }
