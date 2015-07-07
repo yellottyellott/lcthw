62308015bc454eb99a3dbf8adbd231eace9cff75
Erick Yellott <erick.yellott@gmail.com>
2015-07-07 11:31:41 -0400

    ex14: Extra credit.
    Imported `string.h` to use strlen.
    Refactored `can_print_it`.

--- ex14.c
+++ ex14.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
 
 // Forward declarations
-int can_print_it(char ch);
-void print_letters(char arg[]);
+// void print_letters(char arg[]);
+
+
+void print_letters(char arg[]) {
+    int i = 0;
+    char ch;
+    for(i = 0; i < strlen(arg); i++) {
+        ch = arg[i];
+        if(isalpha(ch) || isblank(ch)) {
+            printf("'%c' == %d ", ch, ch);
+        }
+    }
+    printf("\n");
+}
+
 
 void print_arguments(int argc, char *argv[]) {
     int i = 0;
@@ -12,21 +27,6 @@ void print_arguments(int argc, char *argv[]) {
     }
 }
 
-void print_letters(char arg[]) {
-    int i = 0;
-    char ch;
-    for(i = 0; arg[i] != '\0'; i++) {
-        ch = arg[i];
-        if(can_print_it(ch)) {
-            printf("'%c' == %d ", ch, ch);
-        }
-    }
-    printf("\n");
-}
-
-int can_print_it(char ch) {
-    return isalpha(ch) || isblank(ch);
-}
 
 int main(int argc, char *argv[]) {
     print_arguments(argc, argv);
